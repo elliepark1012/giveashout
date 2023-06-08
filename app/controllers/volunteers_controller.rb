56e60ad45a2b962b88caf494d8ba088d7bc34082
Ellie Park <elliepark1012@gmail.com>
2023-06-07 21:45:37 -0400

    routes

--- app/controllers/volunteers_controller.rb
+++ app/controllers/volunteers_controller.rb
@@ -1,2 +1,44 @@
 class VolunteersController < ApplicationController
+    rescue_from ActiveRecord::RecordInvalid, with: :render_unprocessable_entity
+    rescue_from ActiveRecord::RecordNotFound, with: :render_not_found
+
+    def index 
+        render json: Volunteer.all, status: :ok
+    end 
+
+    def show
+        volunteer = Volunteer.find(params[:id])
+        render json: volunteer, include: :contacts, status: :ok
+    end 
+
+    def create
+        volunteer = Volunteer.create!(volunteer_params)
+        render json: volunteer, status: :created
+    end 
+
+    def update 
+        volunteer = Volunteer.find(params[:id])
+        volunteer.update!(volunteer_params)
+        render json: volunteer, status: :accepted
+    end 
+
+    def destroy
+        volunteer = Volunteer.find(params[:id])
+        volunteer.destroy
+        head :no_content 
+    end 
+
+    private
+    
+    def volunteer_params
+        params.permit(:title, :date, :location, :about, :img_url)
+    end 
+
+    def render_unprocessable_entity(invalid)
+        render json: {errors: invalid.record.errors}, status: :unprocessable_entity
+    end 
+
+     def render_not_found(error)
+        render json: {errors: {error.model => "Not Found"}}, status: :not_found
+    end 
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,10 +1,3 @@
 Rails.application.routes.draw do
-  resources :contacts
-  resources :volunteers
-  resources :signups
-  resources :users
-  # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
-
-  # Defines the root path route ("/")
-  # root "articles#index"
+  resources :volunteers, only: [ :index, :show, :create, :update, :destroy]
 end
