84f166da6497b4198cb73fb54bf6b59ae24d4ef2
Ellie Park <elliepark1012@gmail.com>
2023-06-16 12:24:22 -0400

    navbar

--- client/src/components/Navbar.js
+++ client/src/components/Navbar.js
@@ -1,13 +1,16 @@
 import {Link} from 'react-router-dom'
-
+import logo from '../logo.jpeg'
 function NavBar() {
     return (
-        <div>
+        <>
+        <div className="navbar">
             <ul>
-                <li><Link to='/volunteers/new'>New Volunteer</Link></li>
-                <li><Link to='/'> Home</Link></li>
+                <li><Link className="navlink" to='/volunteers/new'>New Volunteer</Link></li>
+                <li><Link className="navlink" to='/'> Home</Link></li>
            </ul>
         </div>
+        <img src={logo}/>
+        </>
     )
 }
 
