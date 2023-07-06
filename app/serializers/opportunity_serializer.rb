6c38a652884a53b32f5f1d1f98f3a0430e25fe60
Ellie Park <elliepark1012@gmail.com>
2023-07-06 16:24:18 -0400

    seriallizer change

--- app/serializers/opportunity_serializer.rb
+++ app/serializers/opportunity_serializer.rb
@@ -1,3 +1,4 @@
 class OpportunitySerializer < ActiveModel::Serializer
   attributes :id, :title, :date, :location, :about, :img_url, :contact_name, :contact_email
+  has_many :users
 end
--- app/serializers/volunteer_serializer.rb
+++
@@ -1,4 +0,0 @@
-class VolunteerSerializer < ActiveModel::Serializer
-  attributes :id, :title, :date, :location, :about, :img_url
-  has_many :users
-end
